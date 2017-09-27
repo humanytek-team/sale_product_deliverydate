35bb5a22470b6cc5cb771625260848ecbd9f41cb
Marco Cid <marcocb46@gmail.com>
2017-09-26 20:38:54 -0700

    se cambia comparacion de fechas

--- sale.py
+++ sale.py
@@ -65,30 +65,66 @@ class SaleOrderLine(models.Model):
                 rec.product_delivery_date = product_delivery_date
         return
 
+
     @api.multi
     def _set_delivery_date(self):
         print '_set_delivery_date'
         for rec in self:
             sale_delay = rec.product_id.sale_delay
             create_date = self.order_id.date_order
-            if create_date:
-                create_date = self.convert_tz(create_date)
+            #if create_date:
+            #    create_date = self.convert_tz(create_date)
             product_delivery_date = False
             if create_date:
+                create_date = self.convert_tz(create_date)
                 create_date = datetime.strptime(str(create_date),"%Y-%m-%d %H:%M:%S")
-                product_delivery_date = timedelta(days=(rec.product_id.sale_delay+1)) + create_date
-                #rec.product_delivery_date = product_delivery_date
-
-                product_delivery_date = product_delivery_date.replace(hour=0,minute=0,second=0)
+                product_delivery_date = create_date
+                #product_delivery_date = rec.order_id.date_order
+                product_delivery_date = datetime.strptime(str(product_delivery_date),"%Y-%m-%d %H:%M:%S")
+                #print 'product_delivery_date: ',product_delivery_date
+                #print 'type product_delivery_date: ',type(product_delivery_date)
+                #product_delivery_date = product_delivery_date.replace(hour=0,minute=0,second=0)
+                product_delivery_date = product_delivery_date +timedelta(days=1)
+                #print 'product_delivery_date: ',product_delivery_date
+                #product_delivery_date = product_delivery_date.strftime("%Y-%m-%d %H:%M:%S")
+                
 
                 new_product_delivery_date = rec.product_delivery_date
                 new_product_delivery_date = datetime.strptime(str(new_product_delivery_date),"%Y-%m-%d")
-                #print 'new_product_delivery_date ',new_product_delivery_date
-                #print 'product_delivery_date ',product_delivery_date
-                if new_product_delivery_date < product_delivery_date:
+                new_product_delivery_date = new_product_delivery_date.replace(hour=23,minute=59,second=59)
+                #print 'new_product_delivery_date: ',new_product_delivery_date
+                if new_product_delivery_date <= product_delivery_date:
+                    
+                    
+                    #print 'product_delivery_date CONVER: ',product_delivery_date
                     #rec.product_delivery_date = product_delivery_date
                     raise ValidationError(_('La fecha de entrega del producto '+rec.product_id.name+' no puede ser menor a '\
                         +str(product_delivery_date)+'.\nPor favor cambiela por otra.'))
+
+    # @api.multi
+    # def _set_delivery_date(self):
+    #     print '_set_delivery_date'
+    #     for rec in self:
+    #         sale_delay = rec.product_id.sale_delay
+    #         create_date = self.order_id.date_order
+    #         if create_date:
+    #             create_date = self.convert_tz(create_date)
+    #         product_delivery_date = False
+    #         if create_date:
+    #             create_date = datetime.strptime(str(create_date),"%Y-%m-%d %H:%M:%S")
+    #             product_delivery_date = timedelta(days=(rec.product_id.sale_delay+1)) + create_date
+    #             #rec.product_delivery_date = product_delivery_date
+
+    #             product_delivery_date = product_delivery_date.replace(hour=0,minute=0,second=0)
+
+    #             new_product_delivery_date = rec.product_delivery_date
+    #             new_product_delivery_date = datetime.strptime(str(new_product_delivery_date),"%Y-%m-%d")
+    #             #print 'new_product_delivery_date ',new_product_delivery_date
+    #             #print 'product_delivery_date ',product_delivery_date
+    #             if new_product_delivery_date < product_delivery_date:
+    #                 #rec.product_delivery_date = product_delivery_date
+    #                 raise ValidationError(_('La fecha de entrega del producto '+rec.product_id.name+' no puede ser menor a '\
+    #                     +str(product_delivery_date)+'.\nPor favor cambiela por otra.'))
 
 
     #product_delivery_date = fields.Date('Fecha de entrega')
