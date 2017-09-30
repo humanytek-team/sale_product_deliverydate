f206c9e47d603d5e01e621434eeaf81f2d73cdba
Marco Cid <marcocb46@gmail.com>
2017-09-29 19:33:44 -0700

    se cambio la formula para calculo de dia de entrega

--- sale.py
+++ sale.py
@@ -60,7 +60,11 @@ class SaleOrderLine(models.Model):
             if create_date:
                 #print 'create_date: ',create_date
                 create_date = datetime.strptime(str(create_date),"%Y-%m-%d %H:%M:%S")
-                product_delivery_date = timedelta(days=(rec.product_id.sale_delay+1)) + create_date
+
+                #product_delivery_date = timedelta(days=(rec.product_id.sale_delay+1)) + create_date
+                product_delivery_date = timedelta(days=(rec.product_id.sale_delay)) + create_date
+
+
                 #print 'product_delivery_date: ',product_delivery_date
                 rec.product_delivery_date = product_delivery_date
         return
@@ -68,7 +72,7 @@ class SaleOrderLine(models.Model):
 
     @api.multi
     def _set_delivery_date(self):
-        print '_set_delivery_date'
+        #print '_set_delivery_date'
         for rec in self:
             sale_delay = rec.product_id.sale_delay
             create_date = self.order_id.date_order
@@ -84,7 +88,9 @@ class SaleOrderLine(models.Model):
                 #print 'product_delivery_date: ',product_delivery_date
                 #print 'type product_delivery_date: ',type(product_delivery_date)
                 #product_delivery_date = product_delivery_date.replace(hour=0,minute=0,second=0)
+
                 product_delivery_date = product_delivery_date +timedelta(days=1)
+
                 #print 'product_delivery_date: ',product_delivery_date
                 #product_delivery_date = product_delivery_date.strftime("%Y-%m-%d %H:%M:%S")
                 
@@ -93,6 +99,7 @@ class SaleOrderLine(models.Model):
                 new_product_delivery_date = datetime.strptime(str(new_product_delivery_date),"%Y-%m-%d")
                 new_product_delivery_date = new_product_delivery_date.replace(hour=23,minute=59,second=59)
                 #print 'new_product_delivery_date: ',new_product_delivery_date
+                #print 'product_delivery_date: ',product_delivery_date
                 if new_product_delivery_date <= product_delivery_date:
                     
                     
